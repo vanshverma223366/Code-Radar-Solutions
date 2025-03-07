7915bdaf20bc876bafdcdc2950f47e21ab024124
vanshverma223366 <vansh0399.becse24@chitkara.edu.in>
2025-03-07 09:31:54 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -8,7 +8,7 @@ int main(){
     }
     int smallestDiff=100,I=0,J=0;
     for (int i=0;i<n;i++){
-        for (int j=i;j<n;j++){
+        for (int j=0;j<n;j++){
         
         if(smallestDiff>array[i]-array[j]&&array[i]-array[j]>0||smallestDiff>array[j]-array[i]&&array[j]-array[i]>0){
             smallestDiff=array[i]-array[j];
